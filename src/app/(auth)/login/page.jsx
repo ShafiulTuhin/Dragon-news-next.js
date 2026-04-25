c6e3af80d8e70505dc2b14bb07b13cdd1d5e84aa
ShafiulTuhin <tuhin6488@gmail.com>
2026-04-26 02:03:37 +0600

    implement logout and google sigin

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,106 +1,11 @@
-"use client";
-import { authClient } from "@/lib/auth-client";
-import Link from "next/link";
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { toast } from "react-toastify";
+import Login from "@/components/login/Login";
+
+export const metadata = {
+  title: "Dragoon News - Login",
+};
 
 const LoginPage = () => {
-  const [isPasswordShow, setIsPasswordShow] = useState(false);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const handleSubmitForm = async (data) => {
-    const { email, password } = data;
-
-    const { data: res, error } = await authClient.signIn.email({
-      email,
-      password,
-      rememberMe: true,
-      callbackURL: "/",
-    });
-
-    if (error) {
-      toast.error(error.message);
-    }
-
-    if (res) {
-      toast.success("sign in successfully");
-    }
-  };
-
-  return (
-    <div className="bg-slate-200 py-20 px-5">
-      <div className="bg-white lg:w-1/2 mx-auto  py-19 rounded-lg px-3 lg:px-0">
-        <h2 className="mb-5 text-center text-[#403F3F] font-semibold lg:text-[35px] text-2xl">
-          Login your account
-        </h2>
-        <form
-          onSubmit={handleSubmit(handleSubmitForm)}
-          className="lg:w-1/2 mx-auto space-y-4"
-        >
-          {" "}
-          <fieldset className="fieldset ">
-            <legend className="fieldset-legend">Email address</legend>
-            <input
-              type="email"
-              className="input  bg-slate-100 w-full"
-              placeholder="email@email.com"
-              {...register("email", {
-                required: "Email field cannot be empty",
-              })}
-            />
-            {errors.email && (
-              <p className="text-red-500 font-semibold">
-                {errors.email.message}
-              </p>
-            )}
-          </fieldset>
-          <fieldset className="fieldset relative">
-            <legend className="fieldset-legend">Password</legend>
-            <div className="flex -gap-10">
-              <input
-                type={isPasswordShow ? "text" : "password"}
-                className="input bg-slate-100 w-full"
-                placeholder="Type here"
-                {...register("password", {
-                  required: "Password cannot be empty",
-                })}
-              />
-              <span
-                onClick={() => setIsPasswordShow(!isPasswordShow)}
-                className="absolute right-2 top-4 cursor-pointer"
-              >
-                {isPasswordShow ? (
-                  <FaEye size={20} />
-                ) : (
-                  <FaEyeSlash size={20} />
-                )}
-              </span>
-            </div>
-
-            {errors.password && (
-              <p className="text-red-500 font-semibold">
-                {errors.password.message}
-              </p>
-            )}
-          </fieldset>
-          <button className="btn bg-pink-500 w-full text-white font-semibold">
-            Login
-          </button>
-        </form>
-        <p className="text-center mt-4">
-          Dont’t Have An Account ?
-          <Link href={"/register"}>
-            <span className="text-pink-500 font-semibold"> Register</span>{" "}
-          </Link>
-        </p>
-      </div>
-    </div>
-  );
+  return <Login />;
 };
 
 export default LoginPage;
